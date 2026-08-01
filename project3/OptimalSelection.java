afe541ae107d59e909573c1dd127a4d31bffe9a5
Elisabeth <enwan.arkanin@hotmail.com>
2026-08-01 11:08:45 -0600

    Update OptimalSelection.java

--- project3/OptimalSelection.java
+++ project3/OptimalSelection.java
@@ -1,5 +1,25 @@
 package programming_project_3;
 
 public class OptimalSelection {
+static final int MAX_WEIGHT = 700;
+
+	static class Experiment {
+		String name;
+		int weight;
+		int rating;
+
+		Experiment(String name, int weight, int rating) {
+			this.name = name;
+			this.weight = weight;
+			this.rating = rating;
+		}
+	}
+
+	static Experiment[] experiments = { new Experiment("Cloud Patterns", 36, 5), new Experiment("Solar Flares", 264, 9),
+			new Experiment("Solar Power", 188, 6), new Experiment("Binary Stars", 203, 8),
+			new Experiment("Relativity", 104, 8), new Experiment("Seed Viability", 7, 4),
+			new Experiment("Sun Spots", 90, 2), new Experiment("Mice Tumors", 65, 8),
+			new Experiment("Microgravity Plant Growth", 75, 5), new Experiment("Micrometeorites", 170, 9),
+			new Experiment("Cosmic Rays", 80, 7), new Experiment("Yeast Fermentation", 27, 4) };
 
 }
